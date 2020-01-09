0f4d355fa537cc32f21b8a286643f8f3560e7f7f
kehinde <bankolek5@gmail.com>
2020-01-10 00:49:31 +0100

    Updates

---
+++ precache-manifest.04f1eefcb7139e327e3754f12de2ce3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a2cf3d199fb3c1f04e475aa00d5ddb",
+    "url": "/reactrmdb/index.html"
+  },
+  {
+    "revision": "9233d429060cef0b89bc",
+    "url": "/reactrmdb/static/css/main.6a178a13.chunk.css"
+  },
+  {
+    "revision": "963c04cc753b27dc9f46",
+    "url": "/reactrmdb/static/js/2.70446411.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactrmdb/static/js/2.70446411.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9233d429060cef0b89bc",
+    "url": "/reactrmdb/static/js/main.42925a87.chunk.js"
+  },
+  {
+    "revision": "01c5abaa6a320236ee69",
+    "url": "/reactrmdb/static/js/runtime-main.c1eaca46.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/reactrmdb/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/reactrmdb/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/reactrmdb/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
